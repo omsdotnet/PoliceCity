fe64d67017954d78dcc9d5ab4fdfec2062697460
Павел Кульбида <kulbida@hotmail.com>
2017-02-28 09:20:25 +0600

    Init repository

---
+++ Sources/BuisnessLevel/GetData.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace BL
+{
+    public partial class GetData : System.Web.UI.Page
+    {
+        
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            HttpRequest rq = this.Request;
+
+            NameValueCollection par = rq.QueryString;
+            string ret = Processing(par);
+
+            Response.Write(ret);
+            Response.End();
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private string Processing(NameValueCollection par)
+        {
+            string[] allKeys = par.AllKeys;
+
+            float a = default(float), l = default(float);
+
+            if (allKeys.Contains("a"))
+            {
+                a = Convert.ToSingle(par["a"], CultureInfo.InvariantCulture.NumberFormat);
+            }
+
+            if (allKeys.Contains("l"))
+            {
+                l = Convert.ToSingle(par["l"], CultureInfo.InvariantCulture.NumberFormat);
+            }
+
+            PointF p = new PointF(a, l);
+
+
+            int? num = null;
+            foreach (Tuple<int, List<PointF>> pl in Global.polList)
+            {
+                if (IsPointInPolygon4(pl.Item2, p))
+                {
+                    num = pl.Item1;
+                }
+            }
+
+            string ret = "alert('не найдено');";
+
+            if (num.HasValue)
+            {
+                string rez = null;
+                Global.dataDict.TryGetValue(num.Value, out rez);
+
+                if (!string.IsNullOrEmpty(rez))
+                {
+                    ret = rez;
+                }
+            }
+
+            
+            return ret;
+        }
+        
+
+
+        public static bool IsPointInPolygon4(IList<PointF> polygon, PointF testPoint)
+        {
+            bool result = false;
+
+            int j = polygon.Count() - 1;
+            for (int i = 0; i < polygon.Count(); i++)
+            {
+                if (polygon[i].Y < testPoint.Y && polygon[j].Y >= testPoint.Y || polygon[j].Y < testPoint.Y && polygon[i].Y >= testPoint.Y)
+                {
+                    if (polygon[i].X + (testPoint.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) * (polygon[j].X - polygon[i].X) < testPoint.X)
+                    {
+                        result = !result;
+                    }
+                }
+                j = i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sources/BuisnessLevel/Global.asax.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.IO;
+using System.Drawing;
+using System.Globalization;
+
+namespace BL
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static List<Tuple<int, List<PointF>>> polList = new List<Tuple<int, List<PointF>>>();
+        public static Dictionary<int, string> dataDict = new Dictionary<int, string>();
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            string[] sep2 = new string[] { "],[" };
+
+
+            string line;
+            FileStream file = new FileStream(Server.MapPath("~\\Data.txt"), FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(file);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] lines2 = line.Split('\t');
+
+                int num = Convert.ToInt32(lines2.First());
+                string[] lines3 = lines2.Last().Split(sep2, StringSplitOptions.RemoveEmptyEntries);
+
+                List<PointF> pointList = new List<PointF>();
+
+                foreach (string str2 in lines3)
+                {
+                    string[] lines4 = str2.Split(',');
+
+                    float X = Convert.ToSingle(lines4.First().Replace("[", string.Empty), CultureInfo.InvariantCulture.NumberFormat);
+                    float Y = Convert.ToSingle(lines4.Last().Replace("]", string.Empty), CultureInfo.InvariantCulture.NumberFormat);
+
+                    pointList.Add(new PointF(X, Y));
+                }
+
+                polList.Add(new Tuple<int, List<PointF>>(num, pointList));            
+            }
+
+            file.Close();
+
+
+            file = new FileStream(Server.MapPath("~\\Data1.txt"), FileMode.Open, FileAccess.Read);
+            sr = new StreamReader(file);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] lines2 = line.Split('\t');
+
+                int num = Convert.ToInt32(lines2.First());
+
+                dataDict.Add(num, lines2.Last());
+            }
+
+            file.Close();
+
+
+        }
+
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpRequest rq = this.Request;
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
