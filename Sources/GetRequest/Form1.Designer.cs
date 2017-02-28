389c8b8c0195c4f7f7a712f14477210081a04ca6
Павел Кульбида <kulbida@hotmail.com>
2017-02-28 09:36:01 +0600

    Init repository

---
+++ Sources/GetRequest/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GetRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.53658F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.46342F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.03261F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.96739F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(656, 368);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(541, 52);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://api-maps.yandex.ru/services/constructor/1.0/js/?sid=FV9NLkQe4dclKnF7bB_KM5" +
+    "qvSQ14cF-V&width=600&height=450";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(550, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox2, 2);
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(3, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(650, 304);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 368);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+
+    }
+}
+
---
+++ Sources/GetRequest/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetRequest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BI.hlpHttp.SinglInstance.Method = "GET";
+            string data = BI.hlpHttp.SinglInstance.SendPost(textBox1.Text, string.Empty);
+
+            int pos1 = data.IndexOf("[[[") + 3;
+            int pos2 = data.IndexOf("}}},[", pos1);
+
+            data = data.Substring(pos1, pos2 - pos1);
+
+            string[] objectsSeparators = new string[] { "]]]},\"name\":\"", "\"coordinates\":[[[" };
+            string[] msgParts = data.Split(objectsSeparators, StringSplitOptions.None);
+
+            string[] coordPairSeparators = new string[] { "],[" };
+            string[] coordSeparators = new string[] { "," };
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < msgParts.Length ; i = i + 2)
+            {
+                sb.Append(msgParts[i+1].Substring(0,3));
+                sb.Append("\t");
+
+                string[] coordsPair = msgParts[i].Split(coordPairSeparators, StringSplitOptions.None);
+                bool First = true;
+                for (int j = coordsPair.Length -1; j >= 0; j--)
+                //for (int j = 0; j < coordsPair.Length; j++)
+                {
+                    if (First)
+                    {
+                        First = false;
+                    }
+                    else
+                    {
+                        sb.Append(",");
+                    }
+
+                    string[] coords = coordsPair[j].Split(coordSeparators, StringSplitOptions.None);
+
+                    sb.Append("[");
+                    sb.Append(coords.Last());
+                    sb.Append(",");
+                    sb.Append(coords.First());
+                    sb.Append("]");
+                }
+
+                sb.Append(Environment.NewLine);
+            }
+
+            data = sb.ToString();
+
+            textBox2.Text = data;
+        }
+    }
+}
